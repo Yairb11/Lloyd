611294da8e34f6abaee7ba40475eacf0859e5027
yairb11 <yairboz70@gmail.com>
2026-09-15 01:18:52 +0300

    Added scanner

--- app/threads/agent.py
+++ app/threads/agent.py
@@ -3,6 +3,7 @@ from PyQt6.QtCore import QThread, pyqtSignal
 import time
 
 from app.threads.manim_render_worker import ManimRenderWorker
+from app.threads.bottle_scan_worker import BottleScanWorker
 from app.config import *
 
 class Agent(QThread):
@@ -18,23 +19,48 @@ class Agent(QThread):
         self.message = message
         self._paused = threading.Event()
         self.manim_worker = None
+        self.bottle_worker = None
 
     def run(self) -> None:
         self.thinking_started.emit()
 
         #===========================================================================================================================================================
-        # TESTING THE AGENT AND THE MANIM VIEW
+        # TESTING THE AGENT
         if self.message.startswith("json"):
             path = self.message[4:]
             with open(path, "r", encoding="utf-8") as file:
                 raw_agent_output = file.read()
             self.generate_and_show_from_string(raw_agent_output)
+        elif self.message.startswith("img"):
+            path = self.message[3:]
+            self.scan_from_image_path(path)
         else:
             time.sleep(1)
             output_messgae = "For a classic Daiquiri, measure 60ml of white rum, 30ml fresh lime juice, and 1/2 oz of 2:1 rich simple syrup."
             self.thinking_ended.emit()
             self.show_reply.emit(output_messgae)
         #===========================================================================================================================================================
+
+    def scan_from_image_path(self, path: str):
+        self.bottle_worker = BottleScanWorker(
+            image_input=path,
+            gpu=False,
+        )
+        self.bottle_worker.scan_finished.connect(self._on_scan_success)
+        self.bottle_worker.scan_failed.connect(self._on_scan_failure)
+
+        self.bottle_worker.start()
+
+    def _on_scan_success(self, output: list):
+        output_messgae = f"Bottles: {",".join(output)}"
+        self.thinking_ended.emit()
+        self.show_reply.emit(output_messgae)
+
+    def _on_scan_failure(self, error_msg: str):
+        output_messgae = f"Scanning failed: {error_msg}"
+        self.thinking_ended.emit()
+        self.show_reply.emit(output_messgae)
+
 
     def generate_and_show_from_string(self, recipe_json_string: str):
         self.manim_worker = ManimRenderWorker(recipe_json_string, self)
